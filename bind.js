3ef1b436d7b234a3baf2d8c96fa57a53c16274eb
Iliya Zhechev <i4ko95@gmail.com>
2017-04-21 15:45:15 +0000

    Initial

---
+++ bind.js
@@ -0,0 +1,86 @@
+class Broadcaster {
+    constructor() {
+        this.observers = new Set();
+    }
+    
+    subscribe(observer) {
+        this.observers.add(observer);
+    }
+    
+    notify(value) {
+        this.observers.forEach(observer => observer.update(value));
+    }
+}
+
+class Variable extends Broadcaster {
+    constructor(initial) {
+        super();
+        this.value = initial;
+    }
+    
+    change(value) {
+        this.set(value);
+        this.notify(value);
+    }
+    
+    set(value) {
+        this.value = value;
+    }
+    
+    get() {
+        return this.value;
+    }
+}
+
+class Bind {
+    constructor(variables = [], initial) {
+        this.variables = new Set(variables);
+        this.value = initial;
+        this.variables.forEach(variable => {
+            initial && variable.change(initial);
+            variable.subscribe({ update: value => {
+                this.variables.delete(variable);
+                this.change(value);
+                this.variables.add(variable);
+            }});
+        });
+    }
+    
+    static all(...[variables]) {
+        return new Bind(variables);
+    }
+    
+    change(value) {
+        this.value = value;
+        this.variables.forEach(variable => variable.set(value));
+    }
+    
+    get() {
+        this.value;
+    }
+}
+
+class DomVariable extends Variable {
+    constructor(element, initial) {
+        super(initial);
+        this.element = element;
+        this.element.addEventListener('input',
+            ({ target }) => this.change(target.value));
+    }
+    
+    static id(identifier) {
+        let element = document.getElementById(identifier);
+        return new DomVariable(element);
+    }
+    
+    set(value) {
+        this.element.value = value;
+        this.value = value;
+    }
+}
+
+Bind.all([
+    DomVariable.id('first'),
+    DomVariable.id('second'),
+    DomVariable.id('third')
+]);
