b5761e17d74f7fca3a35ef6704b9d1f488203940
Iliya Zhechev <i4ko95@gmail.com>
2017-05-06 02:22:09 +0300

    Cleanup

--- src/spa.js
+++ src/spa.js
@@ -1,8 +1,3 @@
-let renderMethodName = 'template';
-let idSeed = 0;
-let getId = () => idSeed++;
-
-
 export class Spa {
     constructor(dom) {
         this.dom = dom;
@@ -29,58 +24,9 @@ export class Spa {
                 typeof item == 'function' ? item(this.dom) : item)))
             .then(raw => raw.forEach(it => this.dom.add(it)));
     }
-    
+
     templateTag(literals, ...values) {
         return literals.reduce((result, literal, id) =>
             result.concat(literal, [...values, ''][id]));
-    }
-}
-
-export class Node {
-    constructor(name) {
-        this.name = name;
-        this.content = new Map();
-    }
-
-    add(content) {
-        let id = getId();
-        this.content[id] = content;
-        return id;
-    }
-    
-    render() {
-        let content = '';
-        this.content.forEach((key, value) => content += value.render());
-
-        this `<${ this.name }>${ content }</${ this.name }>`;
-    }
-}
-
-export class Dom {
-    constructor() {
-        this.container = new Map();
-        this.asyncCnt = 0;
-    }
-    
-    startAsync() {
-        this.asyncCnt++;
-    }
-    
-    endAsync() {
-        this.asyncCnt = this.asyncCnt <= 0 ? this.asyncCnt : --this.asyncCnt;
-    }
-    
-    ready() {
-        return this.asyncCnt === 0;
-    }
-    
-    add(value) {
-        let id = getId();
-        this.set(id, value);
-        return value;
-    }
-
-    set(key, value) {
-        this.container.set(key, value);
     }
 }
