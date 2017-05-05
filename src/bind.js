b8d3d844480eecde1b5e2d72f54acbc6a6e40015
Iliya Zhechev <i4ko95@gmail.com>
2017-05-06 02:22:18 +0300

    Cleanup

--- src/bind.js
+++ src/bind.js
@@ -2,15 +2,15 @@ export class Broadcaster {
     constructor() {
         this.observers = new Set();
     }
-    
+
     subscribe(observer) {
         this.observers.add(observer);
     }
-    
+
     unsubscribe(observer) {
         this.observers.delete(observer);
     }
-    
+
     notify(value) {
         this.observers.forEach(observer => observer.update(value));
     }
@@ -21,7 +21,7 @@ export class Variable extends Broadcaster {
         super();
         this.value = initial;
     }
-    
+
     change(value) {
         if (value != this.value) {
             this.set(value);
@@ -30,20 +30,20 @@ export class Variable extends Broadcaster {
 
         return this;
     }
-    
+
     append(value) {
         return this.change(this.get() + value);
     }
-    
+
     set(value) {
         this.value = value;
         return this;
     }
-    
+
     get() {
         return this.value;
     }
-    
+
     bind(...variables) {
         Bind.all(...variables).with(this);
         return this;
@@ -55,7 +55,7 @@ export class Bind {
         this.variables = new Set();
         this.value = initial;
     }
-    
+
     add(...variables) {
         this.variables = new Set([...this.variables, ...new Set(variables)]);
         variables.forEach(variable => {
@@ -67,16 +67,16 @@ export class Bind {
             }};
             variable.subscribe(this.observer);
         });
-    
+
         return this;
     }
-    
+
     with(variable) {
         this.value = variable.get();
         this.add(variable);
         this.change(this.value);
     }
-    
+
     change(value) {
         this.value = value;
         this.variables.forEach(variable => variable.change(value));
@@ -85,7 +85,7 @@ export class Bind {
     get() {
         this.value;
     }
-    
+
     static all(...variables) {
         return new Bind().add(...variables);
     }
@@ -98,17 +98,17 @@ export class DomVariable extends Variable {
         this.element.addEventListener('input',
             ({ target }) => this.change(target.value));
     }
-    
+
     static select(selector) {
         let element = document.querySelector(selector);
         return new DomVariable(element);
     }
-    
+
     static id(identifier) {
         let element = document.getElementById(identifier);
         return new DomVariable(element);
     }
-    
+
     set(value) {
         if (this.element.value != value) {
             this.element.value = this.value = value;
